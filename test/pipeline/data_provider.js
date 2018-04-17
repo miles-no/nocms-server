2be0517c1106905435a85531595be7fb60b9fbf0
iorg1 <jorgen.landsnes@miles.no>
2018-04-17 10:09:35 +0200

    test: Data provider tests for moved to, exceptions and id lookups

--- test/pipeline/data_provider.js
+++ test/pipeline/data_provider.js
@@ -13,13 +13,31 @@ const config = {
 
 const mockConfig = [
   {
-    pattern: '.*',
+    pattern: 'http://page-service(.*)',
     get: (match, data) => {
       return { status: 200, body: data };
     },
-    fixtures: (match, params, headers, context) => {
+    fixtures: (match) => {
+      if (match[1] === '?uri=%2Fthis-will-fail&site=en') {
+        throw new Error();
+      }
+      if (match[1] === '/page?pageId=123-moved') {
+        return {
+          uri: '/i-am-moved',
+          movedTo: '/moved-to-here',
+          requestedUrl: match ? match[0] : '',
+        };
+      }
+      if (match[1] === '?uri=%2Fi-am-moved&site=en') {
+        return {
+          uri: '/i-am-moved',
+          movedTo: '/moved-to-here',
+          requestedUrl: match ? match[0] : '',
+        };
+      }
       return {
         uri: '/test',
+        requestedUrl: match ? match[0] : '',
       };
     },
   },
@@ -33,8 +51,8 @@ test.after(() => {
   superagentMock.unset();
 });
 
-test.cb('should apply pageData on nocms object', (t) => {
-  const nocms = { 
+test.cb('should apply pageData from page service on nocms object', (t) => {
+  const nocms = {
     logger: console,
     url: '/test',
     config,
@@ -45,12 +63,138 @@ test.cb('should apply pageData on nocms object', (t) => {
   sut
     .fetchData(nocms)
     .then((result) => {
-      t.deepEqual(result.pageData, { uri: nocms.url });
+      t.deepEqual(result.pageData, { uri: nocms.url, requestedUrl: 'http://page-service?uri=%2Ftest&site=en' });
       t.end();
     })
     .catch((ex) => {
       t.fail('Exception was thrown');
       t.log(ex);
+      t.end();
+    });
+});
+
+test.cb('should apply page data from custom data source', (t) => {
+  const nocms = {
+    logger: console,
+    url: '/custom',
+    config,
+    site: 'en',
+    correlationId: '123',
+  };
+
+  const pageData = { uri: '/custom', templateId: 'foo' };
+  sut.addDataSource('/custom', (nocmsObj) => {
+    return new Promise((resolve) => {
+      nocmsObj.pageData = pageData;
+      resolve(nocms);
+    });
+  });
+
+  sut
+    .fetchData(nocms)
+    .then((result) => {
+      t.deepEqual(result.pageData, pageData);
+      t.end();
+    })
+    .catch((ex) => {
+      t.fail('Exception was thrown');
+      t.log(ex);
+      t.end();
+    });
+});
+
+test.cb('should get page data by id if it is set', (t) => {
+  const nocms = {
+    logger: console,
+    pageId: '123',
+    config,
+    site: 'en',
+    correlationId: '123',
+  };
+
+  sut
+    .fetchData(nocms)
+    .then((result) => {
+      t.deepEqual(result.pageData, { uri: '/test', requestedUrl: 'http://page-service/page?pageId=123' });
+      t.end();
+    })
+    .catch((ex) => {
+      t.fail('Exception was thrown');
+      t.log(ex);
+      t.end();
+    });
+});
+
+test.cb('should set exception field if page has been moved', (t) => {
+  const nocms = {
+    logger: console,
+    url: '/i-am-moved',
+    config,
+    site: 'en',
+    correlationId: '123',
+  };
+
+  sut
+    .fetchData(nocms)
+    .then((result) => {
+      t.falsy(result.pageData);
+      t.is(result.redirect, '/moved-to-here');
+      t.is(result.exception.statusCode, 301);
+      t.is(result.exception.location, '/moved-to-here');
+      t.end();
+    })
+    .catch((result) => {
+      t.log(result);
+      t.fail('Shold not throw');
+      t.end();
+    });
+});
+
+test.cb('should not set exception field if page is requested by id', (t) => {
+  const nocms = {
+    logger: console,
+    pageId: '123-moved',
+    config,
+    site: 'en',
+    correlationId: '123',
+  };
+
+  sut
+    .fetchData(nocms)
+    .then((result) => {
+      t.falsy(result.exception);
+      t.truthy(result.pageData);
+      t.falsy(result.redirect);
+      t.is(result.pageData.movedTo, '/moved-to-here');
+      t.is(result.pageData.uri, '/i-am-moved');
+      t.end();
+    })
+    .catch((result) => {
+      t.log(result);
+      t.fail('Shold not throw');
+      t.end();
+    });
+});
+
+test.cb('should set exception field if request fails', (t) => {
+  const nocms = {
+    logger: console,
+    url: '/this-will-fail',
+    config,
+    site: 'en',
+    correlationId: '123',
+  };
+
+  sut
+    .fetchData(nocms)
+    .then((result) => {
+      t.fail('The request shoul throw');
+      t.end();
+    })
+    .catch((result) => {
+      t.truthy(result.exception);
+      t.falsy(result.pageData);
+      t.is(result.exception.statusCode, 500);
       t.end();
     });
 });
